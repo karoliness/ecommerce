2884df34b71a03eb20318d57ad3e7332f84a7e90
KarolineSS <karolinessilva@live.com>
2021-10-18 21:46:16 -0300

    criando teste unitario da classe de autor

---
+++ src/test/java/com/projeto/ecommerce/dominio/AutorTest.java
@@ -0,0 +1,31 @@
+package com.projeto.ecommerce.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+public class AutorTest {
+    private Faker faker = new Faker();
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    public void deveSerObrigatorioOnomeDoautor(String nomeAutor){
+        Exception erroEncontrado = assertThrows(Exception.class, () -> new Autor(nomeAutor));
+        assertEquals("O nome do autor é obrigatório", erroEncontrado.getMessage());
+    }
+
+    @Test
+    public void deveRetornarOnomeDoAutorCorretamente() throws Exception{
+        var nomeAutor = faker.name().fullName();
+
+        var autor = new Autor(nomeAutor);
+
+        assertEquals(nomeAutor, autor.getNome());
+    }
+}
